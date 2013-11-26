372b78a0b2a9b66b7f11120c7b1b3e8922dd1619
Kartik Visweswaran <kartikv2@gmail.com>
2013-11-27 01:42:54 +0530

    Create extended ActiveForm and ActiveField classes

---
+++ widgets/ActiveField.php
@@ -0,0 +1,183 @@
+<?php
+namespace kartik\widgets;
+
+use yii\helpers\Html;
+
+/**
+ * Extends the ActiveField widget to handle various 
+ * bootstrap form types and handle input groups.
+ *
+ * ADDITIONAL VARIABLES/PARAMETERS:
+ * ===============================
+ * @param boolean $hideLabel whether to hide the label for the field (default false)
+ * @param boolean $labelAsPlaceholder whether to display the label as a placeholder (default false)
+ * @param array $addon whether to prepend or append an addon to an input group - contains these keys:
+ * 		- @param string $type whether 'prepend' or 'append'
+ *		- @param string $content the addon content - this is not html encoded
+ *		- @param boolean $asButton whether the addon is a button or button group
+ *
+ * Example(s):
+ * ```php
+ * 	echo $form->field($model, 'email', ['addon' => ['type'=>'prepend', 'content'=>'@']]);
+ * 	echo $form->field($model, 'amount_paid', ['addon' => ['type'=>'append', 'content'=>'.00']]);
+ * 	echo $form->field($model, 'phone', ['addon' => ['type'=>'prepend', 'content'=>'<i class="glyphicon glyphicon-phone']]);
+ * ```
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+
+ 
+/**
+ * @author Kartik Visweswaran <kartik.visweswaran@krajee.com>
+ * @since 2.0
+ 
+ */
+class ActiveField extends \yii\widgets\ActiveField
+{
+	const ADDON_PREPEND = 'prepend';
+	const ADDON_APPEND  = 'append';
+	
+	const TYPE_RADIO    = 'radio';
+	const TYPE_CHECKBOX = 'checkbox';
+	const STYLE_INLINE  = 'inline';
+
+	/**
+	 * @var array addon options for text and password inputs
+	 */
+	public $addon = [];
+	
+	
+	/**
+	 * @var boolean whether the label is to be displayed
+	 */
+	public $hideLabel = false;
+	
+	/**
+	 * @var boolean whether the label is to be displayed as a placeholder
+	 */
+	public $labelAsPlaceholder = false;
+	
+	/**
+	 * @var boolean whether the input is to be offset (like for checkbox or radio).
+	 */
+	private $_offsetInput = false;
+	
+	/**
+	 * Renders the whole field.
+	 * This method will generate the label, error tag, input tag and hint tag (if any), and
+	 * assemble them into HTML according to [[template]].
+	 * @param string|callable $content the content within the field container.
+	 * If null (not set), the default methods will be called to generate the label, error tag and input tag,
+	 * and use them as the content.
+	 * If a callable, it will be called to generate the content. The signature of the callable should be:
+	 *
+	 * ~~~
+	 * function ($field) {
+	 *     return $html;
+	 * }
+	 * ~~~
+	 *
+	 * @return string the rendering result
+	 */
+	public function render($content = null)
+	{
+		$this->initTemplate();
+		$this->initPlaceholder($this->inputOptions);
+		$this->initAddon();
+		return parent::render($content);
+	}
+	
+	/**
+	 * Initializes addon for inputs
+     */	 	
+	protected function initAddon() {
+		if (!empty($this->addon)) {
+			$addon = $this->addon;
+			$type = isset($addon['type']) ? $addon['type'] : self::ADDON_PREPEND;
+			if (isset($addon['asButton']) && $addon['asButton'] == true) {
+				$tag = Html::tag('div', $addon['content'], ['class'=>'input-group-btn']);
+			}
+			else {
+				$tag = Html::tag('span', $addon['content'], ['class'=>'input-group-addon']);
+			}
+			$addonText = ($type == self::ADDON_PREPEND) ? $tag . '{input}' : '{input}' . $tag;
+			$addonText = "<div class='input-group'>{$addonText}</div>";
+			$this->template = str_replace('{input}', $addonText, $this->template);
+		}	
+	}
+	
+	/**
+	 * Initializes placeholder based on $labelAsPlaceholder
+     */	 
+	protected function initPlaceholder(&$options) {
+		if ($this->labelAsPlaceholder) {
+			$label = Html::encode($this->model->getAttributeLabel($this->attribute));
+			$this->inputOptions['placeholder'] = $label;
+			$options['placeholder'] = $label;
+		}
+	}
+	
+	/**
+	 * Initializes template for bootstrap 3 specific styling
+     */	 	
+	protected function initTemplate() {
+		$inputDivClass = $this->form->getInputCss();
+		$offsetDivClass = $this->form->getOffsetCss();
+		if ($this->form->hasInputCss()) {
+			$class = ($this->_offsetInput) ? $offsetDivClass : $inputDivClass;
+			$input = "<div class='{$class}'>{input}</div>";
+			$error = "{error}";
+			$hint = "{hint}";
+			if ($this->form->hasOffsetCss()) {
+				$error = "<div class='{$offsetDivClass}'>{error}</div>";				
+				$hint = "<div class='{$offsetDivClass}'>{hint}</div>";				
+			}
+			$this->template = "{label}\n{$input}\n{$error}\n{$hint}";
+		}
+		if ($this->hideLabel || $this->labelAsPlaceholder) {
+			$this->template = str_replace("{label}\n", "", $this->template);
+		}
+	}
+	
+	/**
+	 * Renders a static input (display only).
+	 * @param array $options the tag options in terms of name-value pairs.
+	 */
+	public function staticInput($options = []) {
+		Html::addCssClass($options, 'form-control-static');
+		$content = isset($this->model[$this->attribute]) ? $this->model[$this->attribute] : '-';
+		$this->parts['{input}'] = Html::tag('p', $content, $options);
+		return $this;
+	}
+	
+	/**
+	 * Renders an input tag.
+	 */
+	public function input($type, $options = [])
+	{
+		if ($type !== 'range') {
+			$options = array_merge($this->inputOptions, $options);
+		}
+		$this->parts['{input}'] = Html::activeInput($type, $this->model, $this->attribute, $options);
+		return $this;
+	}
+	
+	/**
+	 * Renders a checkbox.
+	 */	
+	public function checkbox($options = [], $enclosedByLabel = true) 
+	{
+		$this->_offsetInput = true;
+		return parent::checkbox($options, $enclosedByLabel);
+	}
+	
+	/**
+	 * Renders a radio button
+	 */	
+	public function radio($options = [], $enclosedByLabel = true) 
+	{
+		$this->_offsetInput = true;
+		return parent::checkbox($options, $enclosedByLabel);
+	}
+}
---
+++ widgets/ActiveForm.php
@@ -0,0 +1,188 @@
+<?php
+namespace kartik\widgets;
+use yii\helpers\Html;
+
+/**
+ * Extends the ActiveForm widget to handle various
+ * bootstrap form types.
+ *
+ * Example(s):
+ * ```php
+ * // Horizontal Form
+ * $form = ActiveForm::begin([
+ *		'id' => 'form-signup', 
+ *		'type' => ActiveForm::TYPE_HORIZONTAL
+ * ]);
+ * // Inline Form
+ * $form = ActiveForm::begin([
+ *		'id' => 'form-login', 
+ *		'type' => ActiveForm::TYPE_INLINE
+ *		'fieldConfig' => ['labelAsPlaceholder'=>true]
+ * ]);
+ * // Horizontal Form Configuration
+ * $form = ActiveForm::begin([
+ *		'id' => 'form-signup', 
+ *		'type' => ActiveForm::TYPE_HORIZONTAL
+ *		'formConfig' => ['labelSpan' => 2, 'spanSize' => ActiveForm::SIZE_SMALL]
+ * ]);
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+class ActiveForm extends \yii\widgets\ActiveForm
+{
+	const NOT_SET            = '';
+	const DEFAULT_LABEL_SPAN = 2;
+	const FULL_SPAN          = 12;	// bootstrap full width
+	
+	/* Form Types */
+	const TYPE_VERTICAL		 = 'vertical';
+	const TYPE_HORIZONTAL 	 = 'horizontal';
+	const TYPE_INLINE 		 = 'inline';
+
+	/* Size Modifiers */
+	const SIZE_TINY          = 'xs';
+	const SIZE_SMALL         = 'sm';
+	const SIZE_MEDIUM        = 'md';
+	const SIZE_LARGE         = 'lg';
+
+	/* Label Settings */
+	const SCREEN_READER      = 'sr-only';
+	
+	/**
+	 * @var string form orientation type (for bootstrap styling)
+	 * Defaults to 'vertical'.
+	 */
+	public $type;
+	
+	/**
+	 * @var array the configuration for the form
+	 * 	[
+	 *		'labelSpan'  => 2,	               		 // must be between 1 and 12
+	 *		'spanSize' 	 => ActiveForm::SIZE_MEDIUM, // must be one of the SIZE modifiers
+	 *		'showLabels' => true
+	 *	],
+	 */
+	public $formConfig = [];
+
+	/**
+	 * @var string the extra input container css class for horizontal forms 
+	 * and special inputs like checkbox and radio. 
+	 */
+	private $_inputCss;
+	
+	/**
+	 * @var string the offset class for error and hint for horizontal forms
+	 * or for special inputs like checkbox and radio.
+	 */
+	private $_offsetCss;
+	
+	/**
+	 * @var array the default form configuration
+	 */
+	private $_config = [
+		self::TYPE_VERTICAL => [
+			'labelSpan'  => self::NOT_SET,	// must be between 1 and 12
+			'spanSize' 	 => self::NOT_SET,  // must be one of the SIZE constants set above
+			'showLabels' => true            // show or hide labels (mainly useful for inline type form)
+		],
+		self::TYPE_HORIZONTAL => [
+			'labelSpan'  => self::DEFAULT_LABEL_SPAN,
+			'spanSize' 	 => self::SIZE_MEDIUM, 
+			'showLabels' => true
+		],
+		self::TYPE_INLINE => [
+			'labelSpan'  => self::NOT_SET,
+			'spanSize' 	 => self::NOT_SET, 
+			'showLabels' => false
+		],
+	];
+	
+	/**
+	 * Initializes the form configuration array
+	 * and parameters for the form.
+	 */
+	protected function initForm() {
+		if (!isset($this->type) || strlen($this->type) == 0) {
+			$this->type = self::TYPE_VERTICAL;
+		}
+		if (is_array($this->formConfig) && !empty($this->formConfig)) {
+			$this->_config[$this->type] = array_replace($this->_config[$this->type], $this->formConfig);
+		}
+		if (!isset($this->fieldConfig['class'])) {
+			$this->fieldConfig['class'] = \kartik\widgets\ActiveField::className();
+		}
+		$this->_inputCss = self::NOT_SET;
+		$this->_offsetCss = self::NOT_SET;
+		Html::addCssClass($this->options, 'form-' . $this->type);
+	}
+	
+	/**
+	 * Initializes the widget.
+	 */
+	public function init()
+	{
+		$this->initForm();
+		
+		$config = $this->_config[$this->type];
+		$span = $config['labelSpan'];
+		$size = $config['spanSize'];
+		$labelCss = self::NOT_SET;		
+		
+		if ($span != self::NOT_SET && intval($span) > 0) {
+			$span = intval($span);
+
+			/* Validate if invalid labelSpan is passed - else set to DEFAULT_LABEL_SPAN */
+			if ($span <= 0 && $span >= self::FULL_SPAN) {
+				$span = self::DEFAULT_LABEL_SPAN;
+			}
+
+			/* Validate if invalid spanSize is passed - else default to medium */
+			if ($size == self::NOT_SET) {
+				$size = self::SIZE_MEDIUM;
+			}
+			
+			$prefix = "col-{$size}-";
+			$labelCss = $prefix . $span;
+			$this->_inputCss = $prefix . (self::FULL_SPAN - $span);
+			$this->_offsetCss = "col-" . $size . "-offset-" . $span . " " . $this->_inputCss;
+		}
+		
+		if ($this->_inputCss == self::NOT_SET) {
+			$this->fieldConfig['template'] = "{label}\n{input}\n{error}\n{hint}";
+		}
+		
+		if ($config['showLabels'] === false) {
+			Html::addCssClass($this->fieldConfig['labelOptions'], self::SCREEN_READER);
+		}
+		elseif ($labelCss != self::NOT_SET) {
+			Html::addCssClass($this->fieldConfig['labelOptions'], $labelCss);
+		}
+		
+		parent::init();
+	}
+	
+	public function getInputCss() {
+		return $this->_inputCss;
+	}
+	
+	public function setInputCss($class) {
+		$this->_inputCss = $class;
+	}
+
+	public function hasInputCss() {
+		return ($this->_inputCss != self::NOT_SET);
+	}
+	
+	public function getOffsetCss() {
+		return $this->_offsetCss;
+	}
+
+	public function setOffsetCss($class) {
+		$this->_offsetCss = $class;
+	}
+	
+	public function hasOffsetCss() {
+		return ($this->_offsetCss != self::NOT_SET);
+	}
+}
