1a5ea2ab4677e9e4c18a214f270ad1c6695fc86d
Kartik Visweswaran <kartikv2@gmail.com>
2013-12-08 20:43:39 +0530

    Update affix.js

--- assets/js/affix.js
+++ assets/js/affix.js
@@ -32,7 +32,7 @@
 				top: function () {
 					var offsetTop      = $sideBar.offset().top
 					var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
-					var navOuterHeight = $('.kv-nav').height()
+					var navOuterHeight = $('.kv-header').height()
 
 					return (this.top = offsetTop - navOuterHeight - sideBarMargin)
 				},
