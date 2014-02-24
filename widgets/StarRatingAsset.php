00f1b886e745688737fbfa7ab8d65c214aed7eee
Kartik Visweswaran <kartikv2@gmail.com>
2014-02-25 01:55:22 +0530

    Star Rating widget added

---
+++ widgets/StarRatingAsset.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2013
+ * @package yii2-widgets
+ * @version 1.0.0
+ */
+
+namespace kartik\widgets;
+
+use Yii;
+
+/**
+ * Asset bundle for StarRating Widget
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+class StarRatingAsset extends AssetBundle
+{
+
+    public function init()
+    {
+        $this->setSourcePath(__DIR__ . '/../assets');
+        $this->setupAssets('css', ['css/star-rating']);
+        $this->setupAssets('js', ['js/star-rating']);
+        parent::init();
+    }
+
+}
