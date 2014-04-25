78f7e303f0063ed9f4e40aee0e7617000f9583a0
Kartik Visweswaran <kartikv2@gmail.com>
2014-04-25 23:19:46 +0530

    Upgraded star rating to v2.0

--- widgets/StarRating.php
+++ widgets/StarRating.php
@@ -10,11 +10,18 @@ namespace kartik\widgets;
 
 use Yii;
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+use yii\base\InvalidConfigException;
+use yii\web\View;
+use yii\web\JsExpression;
 
 /**
  * StarRating widget is a wrapper widget for the Bootstrap Star Rating plugin by Krajee.
  * This plugin is a simple star rating yet powerful control that converts a  'number' input
  * to a star rating control using JQuery. The widget is styled for Bootstrap 3.0.
+ *
+ * Upgraded for the new plugin support. Includes fractional ratings with editable star
+ * symbol, RTL inputs, and custom styling.
  *
  * @see http://plugins.krajee.com/star-rating
  * @see http://github.com/kartik-v/bootstrap-star-rating
@@ -61,4 +68,5 @@ class StarRating extends InputWidget
         StarRatingAsset::register($view);
         $this->registerPlugin('rating');
     }
+
 }
