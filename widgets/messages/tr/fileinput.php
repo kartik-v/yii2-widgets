54224492fa72c3ddc1972c45f043c20d7df55c80
Fatih GÜRSOY <fatihgursoy@ymail.com>
2014-07-10 11:51:22 +0300

    Turkish language pack

---
+++ widgets/messages/tr/fileinput.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Message translations for \kartik\widgets\FileInput.
+ *
+ * It contains the localizable messages extracted from source code.
+ * You may modify this file by translating the extracted messages.
+ *
+ * Each array element represents the translation (value) of a message (key).
+ * If the value is empty, the message is considered as not translated.
+ * Messages that no longer need translation will have their translations
+ * enclosed between a pair of '@@' marks.
+ *
+ * Message string can be used with plural forms format. Check i18n section
+ * of the guide for details.
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+return [
+    'Note:' => 'Not:',
+    'multiple file upload' => 'dosyaları yükle',
+    'file preview and multiple file upload' => 'önizle ve dosyaları yükle',
+    'Your browser does not support {validation}. Try an alternative or more recent browser to access these features.' => 'Browserınız  {validation} desteklemiyor. Bu özellikleri kullanabilmek için lütfen alternatif veya güncel bir browser ile bağlanın.',
+    'Browse' => 'Gözat',
+    'Remove' => 'Sil',
+    'Upload' => 'Yükle',
+];
